4ce0e50aaa6198863f1bfd8385cf5822a44d299f
今野颯太 <konno2279@gmail.com>
2017-07-02 01:42:48 +0900

    フィールド(森)

---
+++ Assets/Standard Assets/ParticleSystems/Scripts/Explosive.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+namespace UnityStandardAssets.Effects
+{
+    public class Explosive : MonoBehaviour
+    {
+        public Transform explosionPrefab;
+        public float detonationImpactVelocity = 10;
+        public float sizeMultiplier = 1;
+        public bool reset = true;
+        public float resetTimeDelay = 10;
+
+        private bool m_Exploded;
+ 
+
+
+        // implementing one method from monobehviour to ensure that the enable/disable tickbox appears in the inspector
+  
+       
+
+   
+        
+          
+    }
+}
